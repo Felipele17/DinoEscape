7f0a849c0ba382b2fdeb71b963a8666d9cff24fa
thallissousa <thallissousa@outlook.com>
2022-03-15 17:54:06 -0300

    Criação de labels da tela de settings

--- DinoEscape/DinoEscape Shared/Scenes/SettingsScene.swift
+++ DinoEscape/DinoEscape Shared/Scenes/SettingsScene.swift
@@ -25,43 +25,48 @@ class SettingsScene: SKScene {
         removeAllChildren()
         removeAllActions()
         
-        /*let title: SKLabelNode = SKLabelNode(text: "D I N O")
-        title.fontName = "Aldrich-Regular"
-        title.fontSize = 40
-        title.horizontalAlignmentMode = SKLabelHorizontalAlignmentMode.center
-        title.verticalAlignmentMode = SKLabelVerticalAlignmentMode.center
-        title.numberOfLines = 2
-        title.fontColor = SKColor(red: 235/255, green: 231/255, blue: 198/255, alpha: 1)
-        title.position = CGPoint(x: size.width/2, y: size.height/1.09)
-        addChild(title)
-        */
+        createLabel(text: "Settings",
+                    fontSize: 40,
+                    fontColor: SKColor(red: 235/255, green: 231/255, blue: 198/255, alpha: 1),
+                    position: CGPoint(x: size.width/2, y: size.height/1.30)
+        )
+        
+        createLabel(text: "Sound effects",
+                    fontSize: 25,
+                    fontColor: SKColor(red: 1, green: 1, blue: 1, alpha: 1),
+                    position: CGPoint(x: size.width/2.7, y: size.height/1.5)
+        )
+        
+        createLabel(text: "Sound effects",
+                    fontSize: 25,
+                    fontColor: SKColor(red: 1, green: 1, blue: 1, alpha: 1),
+                    position: CGPoint(x: size.width/2.7, y: size.height/1.5)
+        )
+        
+
+        createLabel(text: "Music",
+                    fontSize: 25,
+                    fontColor: SKColor(red: 1, green: 1, blue: 1, alpha: 1),
+                    position: CGPoint(x: size.width/2.7, y: size.height/1.7)
+        )
     
+        createLabel(text: "Vibration",
+                    fontSize: 25,
+                    fontColor: SKColor(red: 1, green: 1, blue: 1, alpha: 1),
+                    position: CGPoint(x: size.width/2.7, y: size.height/1.96)
+        )
     }
     
-    
-    func createButton(name: ButtonType, pos: Int, titleColor: SKColor) {
-        let texture: SKTexture = SKTexture(imageNamed: "\(name.rawValue)")
-        texture.filteringMode = .nearest
-        let title: SKLabelNode = SKLabelNode(text: "\(name.rawValue)")
-        title.fontName = "Aldrich-Regular"
-        title.fontSize = 20
-        title.fontColor = titleColor
-        
-        let w: CGFloat = size.width / 4.8
-        let h = w * texture.size().height / texture.size().width
-        
-        let button: SKButton = SKButton(texture: texture, color: .clear, size: CGSize(width: w, height: h))
-        
-        button.position = CGPoint(x: button.frame.width * 1 + CGFloat(pos) * button.frame.width * 1.4, y: size.height/5.4)
-        title.position = CGPoint(x: button.frame.width * 1 + CGFloat(pos) * button.frame.width * 1.4, y: size.height/7.8)
-       
-        button.selectedHandler = {
-            print(name)
-        }
-        addChild(button)
-        addChild(title)
+    func createLabel(text: String, fontSize: CGFloat, fontColor: SKColor, position: CGPoint) {
+        let label: SKLabelNode = SKLabelNode(text: text)
+        label.fontName = "Aldrich-Regular"
+        label.fontSize = fontSize
+        label.horizontalAlignmentMode = SKLabelHorizontalAlignmentMode.center
+        label.verticalAlignmentMode = SKLabelVerticalAlignmentMode.center
+        label.fontColor = fontColor
+        label.position = position
+        addChild(label)
     }
-    
     
     override func didChangeSize(_ oldSize: CGSize) {
         super.didChangeSize(oldSize)
