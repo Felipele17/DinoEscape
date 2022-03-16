c29943a58c6c26680bf29af65d6a654557329f0e
Carolina Ortega <cahortega@outlook.com>
2022-03-16 16:39:10 -0300

    Update SettingsScene.swift

--- DinoEscape/DinoEscape Shared/Scenes/SettingsScene.swift
+++ DinoEscape/DinoEscape Shared/Scenes/SettingsScene.swift
@@ -20,54 +20,64 @@ class SettingsScene: SKScene {
     func setUpScene() {
         self.isUserInteractionEnabled = true
         
-        backgroundColor = SKColor(red: 57/255, green: 100/255, blue: 113/255, alpha: 1)
-        
         removeAllChildren()
         removeAllActions()
         
+        let backgroundImage: SKSpriteNode = SKSpriteNode(imageNamed: "homeBackground-iOS")
+        
+        backgroundImage.position = CGPoint(x: size.width/2, y: size.height/2)
+        backgroundImage.size = frame.size
+        backgroundImage.zPosition = -5
+        addChild(backgroundImage)
+        
+        
         createLabel(text: "Settings",
-                    fontSize: 40,
-                    fontColor: SKColor(red: 235/255, green: 231/255, blue: 198/255, alpha: 1),
-                    position: CGPoint(x: size.width/2, y: size.height/1.30)
+                    fontSize: 30,
+                    fontColor: SKColor(red: 57/255, green: 100/255, blue: 113/255, alpha: 1),
+                    position: CGPoint(x: size.width/2, y: size.height/1.35)
         )
         
         createLabel(text: "Sound effects",
-                    fontSize: 25,
-                    fontColor: SKColor(red: 1, green: 1, blue: 1, alpha: 1),
-                    position: CGPoint(x: size.width/2.7, y: size.height/1.5)
+                    fontSize: 23,
+                    fontColor: SKColor(red: 57/255, green: 100/255, blue: 113/255, alpha: 1),
+                    position: CGPoint(x: size.width/2.5, y: size.height/1.55)
         )
         
-        createLabel(text: "Sound effects",
-                    fontSize: 25,
-                    fontColor: SKColor(red: 1, green: 1, blue: 1, alpha: 1),
-                    position: CGPoint(x: size.width/2.7, y: size.height/1.5)
-        )
         
-
         createLabel(text: "Music",
-                    fontSize: 25,
-                    fontColor: SKColor(red: 1, green: 1, blue: 1, alpha: 1),
-                    position: CGPoint(x: size.width/2.7, y: size.height/1.7)
+                    fontSize: 23,
+                    fontColor: SKColor(red: 57/255, green: 100/255, blue: 113/255, alpha: 1),
+                    position: CGPoint(x: size.width/2.5, y: size.height/1.76)
         )
-    
+        
         createLabel(text: "Vibration",
-                    fontSize: 25,
-                    fontColor: SKColor(red: 1, green: 1, blue: 1, alpha: 1),
-                    position: CGPoint(x: size.width/2.7, y: size.height/1.96)
+                    fontSize: 23,
+                    fontColor: SKColor(red: 57/255, green: 100/255, blue: 113/255, alpha: 1),
+                    position: CGPoint(x: size.width/2.5, y: size.height/2.04)
         )
+        
+        createLabel(text: "Back",
+                    fontSize: 23,
+                    fontColor: SKColor(red: 1, green: 1, blue: 1, alpha: 1),
+                    position: CGPoint(x: size.width/2, y: size.height/2.55)
+        )
+        
+        //createButton(name: "Back", pos: CGPoint(x: size.width/2, y: size.height/2.55), titleColor: SKColor(red: 1, green: 1, blue: 1, alpha: 1))
+        
     }
-    
-    func createLabel(text: String, fontSize: CGFloat, fontColor: SKColor, position: CGPoint) {
-        let label: SKLabelNode = SKLabelNode(text: text)
-        label.fontName = "Aldrich-Regular"
-        label.fontSize = fontSize
-        label.horizontalAlignmentMode = SKLabelHorizontalAlignmentMode.center
-        label.verticalAlignmentMode = SKLabelVerticalAlignmentMode.center
-        label.fontColor = fontColor
-        label.position = position
-        addChild(label)
-    }
-    
+        
+        func createLabel(text: String, fontSize: CGFloat, fontColor: SKColor, position: CGPoint) {
+            let label: SKLabelNode = SKLabelNode(text: text)
+            label.fontName = "Aldrich-Regular"
+            label.fontSize = fontSize
+            label.horizontalAlignmentMode = SKLabelHorizontalAlignmentMode.center
+            label.verticalAlignmentMode = SKLabelVerticalAlignmentMode.center
+            label.fontColor = fontColor
+            label.position = position
+            addChild(label)
+        }
+
+        
     override func didChangeSize(_ oldSize: CGSize) {
         super.didChangeSize(oldSize)
         
