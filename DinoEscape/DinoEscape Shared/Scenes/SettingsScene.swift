d3544886827fa9235f1f52673e83062d27df5c70
Carolina Ortega <cahortega@outlook.com>
2022-03-16 17:21:40 -0300

    Update SettingsScene.swift

--- DinoEscape/DinoEscape Shared/Scenes/SettingsScene.swift
+++ DinoEscape/DinoEscape Shared/Scenes/SettingsScene.swift
@@ -37,47 +37,37 @@ class SettingsScene: SKScene {
                     position: CGPoint(x: size.width/2, y: size.height/1.35)
         )
         
-        createLabel(text: "Sound effects",
-                    fontSize: 23,
+        createLabel(text: "  Sound \n effects",
+                    fontSize: 20,
                     fontColor: SKColor(red: 57/255, green: 100/255, blue: 113/255, alpha: 1),
                     position: CGPoint(x: size.width/2.5, y: size.height/1.55)
         )
         
         
         createLabel(text: "Music",
-                    fontSize: 23,
+                    fontSize: 20,
                     fontColor: SKColor(red: 57/255, green: 100/255, blue: 113/255, alpha: 1),
                     position: CGPoint(x: size.width/2.5, y: size.height/1.76)
         )
         
         createLabel(text: "Vibration",
-                    fontSize: 23,
+                    fontSize: 20,
                     fontColor: SKColor(red: 57/255, green: 100/255, blue: 113/255, alpha: 1),
                     position: CGPoint(x: size.width/2.5, y: size.height/2.04)
         )
-        
-        createLabel(text: "Back",
-                    fontSize: 23,
-                    fontColor: SKColor(red: 1, green: 1, blue: 1, alpha: 1),
-                    position: CGPoint(x: size.width/2, y: size.height/2.55)
-        )
-        
-        //createButton(name: "Back", pos: CGPoint(x: size.width/2, y: size.height/2.55), titleColor: SKColor(red: 1, green: 1, blue: 1, alpha: 1))
-        
     }
-        
-        func createLabel(text: String, fontSize: CGFloat, fontColor: SKColor, position: CGPoint) {
-            let label: SKLabelNode = SKLabelNode(text: text)
-            label.fontName = "Aldrich-Regular"
-            label.fontSize = fontSize
-            label.horizontalAlignmentMode = SKLabelHorizontalAlignmentMode.center
-            label.verticalAlignmentMode = SKLabelVerticalAlignmentMode.center
-            label.fontColor = fontColor
-            label.position = position
-            addChild(label)
-        }
-
-        
+    
+    func createLabel(text: String, fontSize: CGFloat, fontColor: SKColor, position: CGPoint) {
+        let label: SKLabelNode = SKLabelNode(text: text)
+        label.fontName = "Aldrich-Regular"
+        label.fontSize = fontSize
+        label.numberOfLines = 2
+        label.horizontalAlignmentMode = SKLabelHorizontalAlignmentMode.center
+        label.verticalAlignmentMode = SKLabelVerticalAlignmentMode.center
+        label.fontColor = fontColor
+        label.position = position
+        addChild(label)
+    }
     override func didChangeSize(_ oldSize: CGSize) {
         super.didChangeSize(oldSize)
         
