97a19db1de8a6b9bb9523c28cf17aeafaf94e4d3
Bianca Maciel <macielmatosb@gmail.com>
2022-03-18 14:57:19 -0300

    Update SKButton.swift

--- DinoEscape/DinoEscape Shared/Nodes/SKButton.swift
+++ DinoEscape/DinoEscape Shared/Nodes/SKButton.swift
@@ -32,11 +32,11 @@ class SKButton: SKSpriteNode {
             switch state {
             case .active:
                 self.isUserInteractionEnabled = true
-                self.alpha = 0.7
+                self.alpha = 1.1
                 break
                 
             case .selected:
-                self.alpha = 1
+                self.alpha = 0.7
                 break
                 
             case .disabled:
