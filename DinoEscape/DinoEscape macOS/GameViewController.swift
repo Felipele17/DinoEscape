683a4ec69ff27c1ebd5ebb7ff411341511bafcf3
thallissousa <thallissousa@outlook.com>
2022-03-21 15:22:18 -0300

    Pequenas alterações no posicionamento dos botões no macOS e tvOS

--- DinoEscape/DinoEscape macOS/GameViewController.swift
+++ DinoEscape/DinoEscape macOS/GameViewController.swift
@@ -14,7 +14,7 @@ class GameViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let scene = GameScene.newGameScene()
+        let scene = SettingsScene.newGameScene()
         
         // Present the scene
         let skView = self.view as! SKView
