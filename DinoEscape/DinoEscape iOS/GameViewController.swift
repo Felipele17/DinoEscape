e57002849eb4c3a02f5c0652744bc0418154c6d4
raphaelalkamim <83611337+raphaelalkamim@users.noreply.github.com>
2022-03-21 17:04:06 -0300

    pause scene implemented

--- DinoEscape/DinoEscape iOS/GameViewController.swift
+++ DinoEscape/DinoEscape iOS/GameViewController.swift
@@ -15,8 +15,8 @@ class GameViewController: UIViewController {
         super.viewDidLoad()
         
         //let scene = GameScene.newGameScene()
-        let scene = HomeScene.newGameScene()
-        //let scene = StoreScene.newGameScene()
+       // let scene = HomeScene.newGameScene()
+        let scene = SettingsPopUpScene.newGameScene()
         
         
         // Present the scene
