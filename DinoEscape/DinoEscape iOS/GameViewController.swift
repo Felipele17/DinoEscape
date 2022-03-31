53940e2720dd704ffd8e113f4c91f66ced962dee
Felipe Torales Leite <felipetolei@hotmail.com>
2022-03-31 19:13:32 -0300

    Mudanças para mandar para AppStore

--- DinoEscape/DinoEscape iOS/GameViewController.swift
+++ DinoEscape/DinoEscape iOS/GameViewController.swift
@@ -26,10 +26,6 @@ class GameViewController: UIViewController {
         skView.presentScene(scene)
         
         skView.ignoresSiblingOrder = true
-        skView.showsFPS = true
-        skView.showsNodeCount = true
-        
-
     }
 
     
