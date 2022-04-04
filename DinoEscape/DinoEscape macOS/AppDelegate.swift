cb1ed5990a733cf222c9dc12b2a54194fa8c679c
Luca Hummel <luca.hummel12@gmail.com>
2022-04-04 15:11:45 -0300

    Fazer com que o app quit quando clica no X

--- DinoEscape/DinoEscape macOS/AppDelegate.swift
+++ DinoEscape/DinoEscape macOS/AppDelegate.swift
@@ -22,7 +22,5 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
         return true
     }
-
-
 }
 
